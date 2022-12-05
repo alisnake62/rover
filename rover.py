5588690215b67cbc54f4f976c42483e6a7ae1809
dremaux <ldremaux@btslivh.eu>
2022-12-05 10:42:40 +0100

    Ajout de la classe "Position" et "Déplacement" ( elles ne sont pas fini )

--- rover.py
+++ rover.py
@@ -20,6 +20,57 @@ class Orientation:
     def tournedroite(self):
         if self._cardinal == Cardinal("Sud"): return Cardinal("Ouest")
 
+class Position:
+
+    #j'ai besoin que tu finisses Orientation() ^^
+    
+
+    def __init__(self, value):
+        self._value = value
+        x = 5
+        y = 5
+        
+    def __eq__(self, otherPosition):
+        return self._value == otherPosition._value
+
+    # trouver ou le mettre ( on va pas appeler la methode à chaque fois)
+    def limite_depasser(self):
+        # a enlever (je le met là juste pour enlever les erreurs)
+        x = 5
+        y = 5
+        #
+        if x > 10: return x == 10
+        if x < 0: return x == 0
+        if y > 10: return y == 0
+        if y < 0: return y == 10
+
+class Deplacement:
+
+    def __init__(self, cardinal:Cardinal, position:Position):
+        self._cardinal = cardinal
+        self._position = position
+    
+    def avance(self):
+        # a enlever
+        x = 5
+        y = 5
+        #
+        if self._cardinal == Cardinal("Sud"): return y == y - 1
+        if self._cardinal == Cardinal("Nord"): return y == y + 1 
+        if self._cardinal == Cardinal("Est"): return x == x + 1
+        if self._cardinal == Cardinal("Ouest"): return x == x - 1
+
+    def recule(self):
+        # a enlever
+        x = 5
+        y = 5
+        #
+        if self._cardinal == Cardinal("Sud"): return y == y + 1 
+        if self._cardinal == Cardinal("Nord"): return y == y - 1
+        if self._cardinal == Cardinal("Est"): return x == x - 1
+        if self._cardinal == Cardinal("Ouest"): return x == x + 1
+
+
 class Rover:
 
     def __init__(self, orientation: Orientation):
