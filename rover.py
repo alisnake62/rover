b281ad4e373304427c1303961f5092771332923e
dremaux <ldremaux@btslivh.eu>
2022-12-05 11:57:34 +0100

    modification de la classe Position et Deplacement

--- rover.py
+++ rover.py
@@ -20,31 +20,17 @@ class Orientation:
     def tournedroite(self):
         if self._cardinal == Cardinal("Sud"): return Cardinal("Ouest")
 
-class Position:
+class Position(Orientation):
 
     #j'ai besoin que tu finisses Orientation() ^^
     
-
-    def __init__(self, value):
-        self._value = value
-        x = 5
-        y = 5
+    def __init__(self, cordonner):
+        self._cordonner = cordonner
         
     def __eq__(self, otherPosition):
-        return self._value == otherPosition._value
+        return self._cordonner == otherPosition._cordonner
 
-    # trouver ou le mettre ( on va pas appeler la methode à chaque fois)
-    def limite_depasser(self):
-        # a enlever (je le met là juste pour enlever les erreurs)
-        x = 5
-        y = 5
-        #
-        if x > 10: return x == 10
-        if x < 0: return x == 0
-        if y > 10: return y == 0
-        if y < 0: return y == 10
-
-class Deplacement:
+class Deplacement(Position):
 
     def __init__(self, cardinal:Cardinal, position:Position):
         self._cardinal = cardinal
@@ -59,6 +45,10 @@ class Deplacement:
         if self._cardinal == Cardinal("Nord"): return y == y + 1 
         if self._cardinal == Cardinal("Est"): return x == x + 1
         if self._cardinal == Cardinal("Ouest"): return x == x - 1
+        if x > 10: return x == 10
+        if x < 0: return x == 0
+        if y > 10: return y == 0
+        if y < 0: return y == 10
 
     def recule(self):
         # a enlever
@@ -69,6 +59,10 @@ class Deplacement:
         if self._cardinal == Cardinal("Nord"): return y == y - 1
         if self._cardinal == Cardinal("Est"): return x == x - 1
         if self._cardinal == Cardinal("Ouest"): return x == x + 1
+        if x > 10: return x == 10
+        if x < 0: return x == 0
+        if y > 10: return y == 0
+        if y < 0: return y == 10
 
 
 class Rover:
