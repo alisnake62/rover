552f83141b7df300afa123154683a45c0ff50d62
dremaux <ldremaux@btslivh.eu>
2022-12-06 16:57:50 +0100

    modification de la classe orientation | teste pour réparer mes conneries
    comme y a l'ajout d'un nouveau paramètre rover doit aussi le prendre ! correctement !

--- rover.py
+++ rover.py
@@ -11,62 +11,55 @@ class Cardinal():
     def __eq__(self, otherCardinal):
         return self._value == otherCardinal._value
 
-class Orientation:
+class Orientation():
 
     def __init__(self, cardinal:Cardinal):
         self._cardinal = cardinal
 
+    #besoin d'explication pour cette methode
     def __eq__(self, otherOrientation):
         return self._cardinal == otherOrientation._cardinal
 
-    def tournedroite(self):
+    def tourne_droite(self):
         if self._cardinal == Cardinal("Sud"): return Cardinal("Ouest")
+        if self._cardinal == Cardinal("Ouest"): return Cardinal("Nord")
+        if self._cardinal == Cardinal("Nord"): return Cardinal("Est")
+        if self._cardinal == Cardinal("Est"): return Cardinal("Sud")
 
+    def tourne_gauche(self):
+        if self._cardinal == Cardinal("Sud"): return Cardinal("Est")
+        if self._cardinal == Cardinal("Est"): return Cardinal("Nord")
+        if self._cardinal == Cardinal("Nord"): return Cardinal("Ouest")
+        if self._cardinal == Cardinal("Ouest"): return Cardinal("Sud")
 
-
-class Position:
+class Position():
     
-    def __init__(self, y=0, x=0):
-        self._y = y
-        self._x = x
-        self._position = x, y
+    def __init__(self, y, x):
+        self.axe_y = y
+        self.axe_x = x
+        self._position = self.axe_y, self.axe_x
         
     def __eq__(self, otherPosition):
         return self._position == otherPosition._position
     
     def avance(self):
-        if self._cardinal == Cardinal("Sud"): return self.y == self.y - 1
-        if self._cardinal == Cardinal("Nord"): return self.y == self.y + 1
-        if self._cardinal == Cardinal("Est"): return self.x == self.x + 1
-        if self._cardinal == Cardinal("Ouest"): return self.x == self.x - 1
-        if self.x > 10: return self.x == 10
-        if self.x < 0: return self.x == 0
-        if self.y > 10: return self.y == 0
-        if self.y < 0: return self.y == 10
-
-        #je suis pas sure du return ._. mais il faut récuperer position
-        return self._position == self.x, self.y
-
-    def recule(self):
-        if self._cardinal == Cardinal("Sud"): return self.y == self.y + 1
-        if self._cardinal == Cardinal("Nord"): return self.y == self.y - 1
-        if self._cardinal == Cardinal("Est"): return self.x == self.x - 1
-        if self._cardinal == Cardinal("Ouest"): return self.x == self.x + 1
-        if self.x > 10: return self.x == 10
-        if self.x < 0: return self.x == 0
-        if self.y > 10: return self.y == 0
-        if self.y < 0: return self.y == 10
-
-        #je suis pas sure du return ._. mais il faut récuperer position
-        return self._position == self.x, self.y
+        if self._cardinal == Cardinal("Sud"): return self.axe_y == self.axe_y - 1
+        if self._cardinal == Cardinal("Nord"): return self.axe_y == self.axe_y + 1
+        if self._cardinal == Cardinal("Est"): return self.axe_x == self.axe_x + 1
+        if self._cardinal == Cardinal("Ouest"): return self.axe_x == self.axe_x - 1
+        if self.axe_x > 10: return self.axe_x == 10
+        if self.axe_x < 0: return self.axe_x == 0
+        if self.axe_y > 10: return self.axe_y == 0
+        if self.axe_y < 0: return self.axe_y == 10
+        self._position == self.axe_x, self.axe_y
 
 
-class Rover:
+class Rover():
 
     def __init__(self, orientation: Orientation, position: Position):
         self._orientation = orientation
         self._position = position
-    
+        
     def getOrientation(self):
         return self._orientation
 
@@ -74,7 +67,15 @@ class Rover:
         return self._position
 
     def tournedroite(self):
-        self._orientation.tournedroite()
+        self._orientation.tourne_droite()
 
     def avance(self):
         self._position.avance()
+
+
+##########   tests   ###########
+# rover = Rover("Nord",(5,5))
+# print(rover._orientation, rover._position)
+# rover.avance
+# rover.tournedroite
+# print(rover._orientation, rover._position)
