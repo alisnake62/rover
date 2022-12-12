6d850a7723d97cfaa4c91c602cce682c535fdf50
dremaux <ldremaux@btslivh.eu>
2022-12-12 11:30:36 +0100

    Classe fait mais regarde si bien fait (j'ai plus les règles sous les yeux), et teste à finir et non fonctionnelle, à voir pourquoi

--- rover.py
+++ rover.py
@@ -1,15 +1,15 @@
-class Cardinal():
+# class Cardinal():
 
-    Sud = "Sud"
-    Ouest = "Ouest"
-    Est = "Est"
-    Nord = "Nord"
+#     Sud = "Sud"
+#     Ouest = "Ouest"
+#     Est = "Est"
+#     Nord = "Nord"
 
-    def __init__(self, value):
-        self._value = value
+#     def __init__(self, value):
+#         self._value = value
 
-    def __eq__(self, otherCardinal):
-        return self._value == otherCardinal._value
+#     def __eq__(self, otherCardinal):
+#         return self._value == otherCardinal._value
 
 # class Orientation():
 
@@ -26,73 +26,93 @@ class Cardinal():
 
 class Position():
     
-    def __init__(self, point_y, point_x, cardinal: Cardinal):
-        self._cardinal = cardinal
+    def __init__(self, point_y, point_x, cardinal):
+        self.cardinal = cardinal
         self.axe_y = point_y
         self.axe_x = point_x
 
     def position(self):
-        print(self.axe_y, self.axe_x, self._cardinal)
+        print(self.axe_y, self.axe_x, self.cardinal)
 
 class Deplacement(Position):
 
     def avance(self):
         #avancement enfonction de la direction
-        if self._cardinal == "Sud":     self.axe_y += -1
-        if self._cardinal == "Nord":    self.axe_y += 1
-        if self._cardinal == "Est":     self.axe_x += 1
-        if self._cardinal == "Ouest":   self.axe_x += -1
+        if self.cardinal == "Sud":     self.axe_y += -1
+        if self.cardinal == "Nord":    self.axe_y += 1
+        if self.cardinal == "Est":     self.axe_x += 1
+        if self.cardinal == "Ouest":   self.axe_x += -1
 
         #verification qu'il soit toujours sur la map
-        if self.axe_x > 10: self.axe_x == 10
-        if self.axe_x < 0:  self.axe_x == 0
-        if self.axe_y > 10: self.axe_y == 0
-        if self.axe_y < 0:  self.axe_y == 10
+        if self.axe_x > 10: self.axe_x = 10
+        if self.axe_x < 0:  self.axe_x = 0
+        if self.axe_y > 10: self.axe_y = 0
+        if self.axe_y < 0:  self.axe_y = 10
 
     def recule(self):
         #recule enfonction de la direction
-        if self._cardinal == "Sud":     self.axe_y += 1
-        if self._cardinal == "Nord":    self.axe_y += -1
-        if self._cardinal == "Est":     self.axe_x += -1
-        if self._cardinal == "Ouest":   self.axe_x += 1
+        if self.cardinal == "Sud":     self.axe_y += 1
+        if self.cardinal == "Nord":    self.axe_y += -1
+        if self.cardinal == "Est":     self.axe_x += -1
+        if self.cardinal == "Ouest":   self.axe_x += 1
 
         #verification qu'il soit toujours sur la map
-        if self.axe_x > 10: self.axe_x == 10
-        if self.axe_x < 0:  self.axe_x == 0
-        if self.axe_y > 10: self.axe_y == 0
-        if self.axe_y < 0:  self.axe_y == 10
+        if self.axe_x > 10: self.axe_x = 10
+        if self.axe_x < 0:  self.axe_x = 0
+        if self.axe_y > 10: self.axe_y = 0
+        if self.axe_y < 0:  self.axe_y = 10
 
     def tourne_droite(self):
+        test=1  
         #change de direction vers la droite
-        if self._cardinal == "Sud":     self._cardinal == "Ouest"
-        if self._cardinal == "Ouest":   self._cardinal == "Nord"
-        if self._cardinal == "Nord":    self._cardinal == "Est"
-        if self._cardinal == "Est":     self._cardinal == "Sud"
+        if self.cardinal == "Sud" and test == 1:     
+            self.cardinal = "Ouest" 
+            test = 2
+        if self.cardinal == "Ouest" and test == 1:   
+            self.cardinal = "Nord"
+            test = 2
+        if self.cardinal == "Nord" and test == 1:    
+            self.cardinal = "Est"
+            test = 2
+        if self.cardinal == "Est" and test == 1:     
+            self.cardinal = "Sud"
+            test = 2
 
     def tourne_gauche(self):
+        test=1  
         #change de direction vers la gauche
-        if self._cardinal == "Sud":     self._cardinal == "Est"
-        if self._cardinal == "Est":     self._cardinal == "Nord"
-        if self._cardinal == "Nord":    self._cardinal == "Ouest"
-        if self._cardinal == "Ouest":   self._cardinal == "Sud"
+        if self.cardinal == "Sud" and test == 1: 
+            self.cardinal = "Est" 
+            test = 2
+        if self.cardinal == "Est" and test == 1:     
+            self.cardinal = "Nord"
+            test = 2
+        if self.cardinal == "Nord" and test == 1:    
+            self.cardinal = "Ouest"
+            test = 2
+        if self.cardinal == "Ouest" and test == 1:   
+            self.cardinal = "Sud"
+            test = 2
 
 
 class Rover(Deplacement):
 
     def __init__(self):
-        self._cardinal = "Nord"
+
+        #direction entre ( Nord | Sud | Est | Ouest )
+        self.cardinal = "Nord"
+
+        #axe Y n'import qu'elle chiffre 
         self.axe_y = 5
+
+        #axe X n'import qu'elle chiffre 
         self.axe_x = 5
 
 
 ###########   test   ############
-rover = Rover()
-
-rover.position()
-
-rover.tourne_droite()
-
-rover.position()
-
-
+# rover = Rover()
+# print("Y|X|Direction")
+# rover.position()
+# rover.avance()
+# rover.position()
 
--- test_rover.py
+++ test_rover.py
@@ -1,6 +1,6 @@
 import unittest
 
-from rover import Rover, Orientation, Position
+from rover import Rover
 
 class TestRover(unittest.TestCase):
 
@@ -18,15 +18,70 @@ class TestRover(unittest.TestCase):
 
     def test_avance(self):
         # Arrange
-        rover = Rover(orientation=Position())
+        rover = Rover()
+        rover.axe_x = 5
+        rover.axe_y = 5
+        self.cardinal = "Nord"
 
         # Action
         rover.avance()
         
         # Assert
-        position_actuel = rover.getPosition()
-        self.assertEqual(position_actuel, Position(10,0))
+        self.assertEqual("6 5 Nord", rover.position())
+
+    def test_recule(self):
+        # Arrange
+        rover = Rover()
+        rover.axe_x = 5
+        rover.axe_y = 5
+        self.cardinal = "Nord"
+
+        # Action
+        rover.avance()
         
+        # Assert
+        self.assertEqual("4 5 Nord", rover.position())
+
+    def test_limite_map(self):
+        # Arrange
+        rover = Rover()
+        rover.axe_x = 10
+        rover.axe_y = 5
+        self.cardinal = "Nord"
+
+        # Action
+        rover.avance()
+        
+        # Assert
+        self.assertEqual("0 5 Nord", rover.position())
+
+    def test_tourne_gauche(self):
+        # Arrange
+        rover = Rover()
+        rover.axe_x = 5
+        rover.axe_y = 5
+        self.cardinal = "Nord"
+
+        # Action
+        rover.tourne_gauche()
+        
+        # Assert
+        self.assertEqual("5 5 Ouest", rover.position())
+
+    def test_tourne_droite(self):
+        # Arrange
+        rover = Rover()
+        rover.axe_x = 5
+        rover.axe_y = 5
+        self.cardinal = "Nord"
+
+        # Action
+        rover.tourne_droite()
+        
+        # Assert
+        self.assertEqual("5 5 Est", rover.position())
+        
+    
 
 
 if __name__ == '__main__':
